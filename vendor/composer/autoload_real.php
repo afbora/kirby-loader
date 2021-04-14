26db2602c4bb2b3588bbf0525c24cd67d3bec09a
Ahmet Bora <byybora@gmail.com>
2021-04-14 22:26:53 +0300

    v1.4.0

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -25,7 +25,7 @@ class ComposerAutoloaderInit3fb0a4d64201676d2dcd03e06dd74f70
         require __DIR__ . '/platform_check.php';
 
         spl_autoload_register(array('ComposerAutoloaderInit3fb0a4d64201676d2dcd03e06dd74f70', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit3fb0a4d64201676d2dcd03e06dd74f70', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
